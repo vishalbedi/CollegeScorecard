6d6e549868924c3129a50f02f8739f16194f0a4e
Matthew Connelly <mrc3340@rit.edu>
2017-04-28 13:46:26 -0400

    Added some Comments

--- backend/flask_app/models.py
+++ backend/flask_app/models.py
@@ -6,7 +6,8 @@ from sqlalchemy import text
 
 db = SQLAlchemy()
 
-
+# Queries the dataset to get the number of Colleges each year has
+# Returns a Collection containing the number of colleges by year
 def getCollegesPerYear():
     CollegesPerYear = "SELECT Year, COUNT(Id) NumSchools FROM Scorecard GROUP BY Year"
     sql = text(CollegesPerYear)
@@ -16,6 +17,9 @@ def getCollegesPerYear():
         names.append([row[0], row[1]])
     return names
 
+# Queries the dataset to get the Ethnicity data on a each College in the dataset
+# Will return the number of students in each college, how many of these students are White, Black,
+# Hispanic, etc..
 def getCollegesByEthnicity():
     CollegesByEthnicity = "SELECT unitID, College, UGDS, UGDS_WHITE, UGDS_BLACK, UGDS_HISP, UGDS_ASIAN, UGDS_AIAN, UGDS_NHPI, UGDS_2MOR, UGDS_NRA, UGDS_UNKN, probSchool, totprob FROM Scorecard"
     sql = text(CollegesByEthnicity)
@@ -25,6 +29,8 @@ def getCollegesByEthnicity():
         rows.append([row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9], row[10], row[11], row[12], row[13]])
     return rows
 
+# Queries the dataset to get student aid information on each college in the dataset
+# it then returns this data
 def getCollegesByStudentAid():
     UGDSSumCommand = "SELECT SUM(UGDS) from Scorecard"
     sql = text(UGDSSumCommand)
@@ -36,7 +42,6 @@ def getCollegesByStudentAid():
     sql = text(CollegesByStudentAid)
     result = db.engine.execute(sql)
     rows = [["unitID", "College", "UGDS", "pell_ever_2005", "fsend_1_2005", "fsend_2_2005", "fsend_3_2005", "fsend_4_2005", "fsend_5_2005", "probSchool", "totprob"]]
-    
     for row in result:
         rows.append([row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[2]/UGDSSum, (row[4] + row[5] + row[6] + row[7] + row[8])])
     return rows
