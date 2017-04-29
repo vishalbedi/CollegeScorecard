b3574a5485bd11a9b0376b68bd88c1cb30fbedde
Vishal <vishal.b1989@gmail.com>
2017-04-28 20:50:08 -0400

    UI completed

---
+++ front/src/app/home/Colleges.ts
@@ -0,0 +1,12 @@
+/**
+ * Created by vishal on 4/28/17.
+ */
+export class College {
+  constructor(
+    public name: string,
+    public url: string,
+    public npcurl: string,
+    public numOfUdgr: number,
+    public satAvg: number
+  ) {  }
+}
--- front/src/app/home/home.component.ts
+++ front/src/app/home/home.component.ts
@@ -2,51 +2,41 @@ import { Component, OnInit } from '@angular/core';
 import { Response } from '@angular/http';
 import { WebService } from '../webservices';
 import { User }    from './User';
-
+import { College } from './Colleges';
 @Component({
   selector: 'home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
   providers: [WebService]
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent  {
   public genders = ['Male', 'Female'];
   public regions = ['Far West', 'Great Lakes', 'Mid East', 'New England', 'Plains',
     'Rocky Mountains', 'Southeast', 'Southwest'];
   public model = new User('', 600, 600, 600, '', '');
   public submitted: boolean = false;
-  public collegesCharOptions: any =  {
-    chartType: 'ColumnChart',
-    dataTable: [],
-    options: {title: 'Colleges Per Year'}
-  };
+  public colleges = [];
   constructor(private webservice: WebService) { }
-
-  public ngOnInit() {
-    this.getData();
-  }
-
   public onSubmit() {
-    this.submitted = true;
-    this.webservice.postResource(JSON.stringify(this.model), '/somewhere').subscribe();
-  }
-  /**
-   * Fetch the data from the python-flask backend
-   */
-  private getData() {
-    this.webservice.getCollegesPerYear()
+    this.webservice.postResource(JSON.stringify(this.model), '/api/analytics/get-college-list')
       .subscribe(
         (data) => this.handleData(data),
         console.log,
         () => console.log('got data')
       );
   }
+  /**
+   * Fetch the data from the python-flask backend
+   */
 
    private handleData(data: Response) {
     if (data.status === 200) {
       let receivedData = data.json();
-      this.collegesCharOptions.dataTable = receivedData;
+      for (let i = 1; i < receivedData.length; i++) {
+        this.colleges.push(new College(receivedData[i][0], receivedData[i][1], receivedData[i][2],
+          receivedData[i][3], receivedData[i][4]));
+      }
+      this.submitted = true;
     }
-    console.log(this.collegesCharOptions.dataTable);
   }
 }
